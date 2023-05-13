dab181df192f93139bbc90bcb67bf081c3aa2e5e
LuckyMirik <Luckymirik@mail.ru>
2023-05-13 16:11:12 +0300

    Закончил ДЗ

--- src/Book.java
+++ src/Book.java
@@ -1,14 +1,22 @@
 public class Book {
     private String nameBook;
+
     private Author author;
+
     private int yearOfIssue;
-    public Book(String nameBook,int yearOfIssue){
+    public Book(String nameBook,Author author,int yearOfIssue){
         this.nameBook=nameBook;
+        this.author =author;
         this.yearOfIssue = yearOfIssue;
+
 
     }
     public String getNameBook(){
         return this.nameBook;
+    }
+    public  Author getAuthor() {
+
+        return author;
     }
 
     public int getYearOfIssue() {
@@ -18,4 +26,8 @@ public class Book {
     public void setYearOfIssue(int yearOfIssue) {
         this.yearOfIssue = yearOfIssue;
     }
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Author kats = new Author("Максим", "Кац");
-        System.out.println(kats.getNameAuthor());
-        System.out.println(kats.getSurname());
-        Book history = new Book("История новой России", 2020);
+        Author kats =new Author("Максим", "Кац");
+        Book history = new Book("История новой России",kats,2020);
+        System.out.println(history.getAuthor().getNameAuthor());
+        System.out.println(history.getAuthor().getSurname());
         System.out.println(history.getNameBook());
         System.out.println(history.getYearOfIssue());
         history.setYearOfIssue(2021);
@@ -12,12 +12,12 @@ public class Main {
 
         System.out.println();
 
-        Author tinkof = new Author("Олег", "Тиньков");
-        System.out.println(tinkof.getNameAuthor());
-        System.out.println(tinkof.getSurname());
-        Book asEverybody = new Book("Я такой как все", 2010);
+        Author tinkof = new Author("Олег",  "Тиньков");
+        Book asEverybody = new Book("Я такой как все",tinkof, 2010);
         System.out.println(asEverybody.getNameBook());
         System.out.println(asEverybody.getYearOfIssue());
+        System.out.println(asEverybody.getAuthor().getNameAuthor());
+        System.out.println(asEverybody.getAuthor().getSurname());
 
     }
 
